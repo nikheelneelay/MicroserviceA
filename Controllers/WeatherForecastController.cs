0ba4f11213319f974f7be1c8d5b77597cd80169f
Nikheel Neelay <P10496648@capita.co.uk>
2019-10-13 23:32:28 +0530

    Add project files.

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MicroserviceA.Data;
+using MicroserviceA.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace MicroserviceA.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        public WeatherForecastController(SchoolContext context)
+        {
+            Context = context;
+        }
+        private static readonly string[] Summaries = new[]
+        {
+           Environment.MachineName, "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public SchoolContext Context { get; }
+
+       
+
+        [HttpGet]
+        public IEnumerable<Student> Get()
+        {
+            var rng = new Random();
+
+           var Students =  Context.Students.ToList();
+
+            return Students;
+
+            //return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            //{
+            //    Date = DateTime.Now.AddDays(index),
+            //    TemperatureC = rng.Next(-20, 55),
+            //    Summary = Summaries[rng.Next(Summaries.Length)]
+            //})
+            //.ToArray();
+        }
+    }
+}
